45e1ed0d243793dda59d1c1230a9eda5deee9b57
Shoichi Sasagawa <115807586+CITShoichiSasagawa@users.noreply.github.com>
2022-10-28 08:16:18 +0000

    .replit編集 test2.dbにデータ追加

--- insert_all.js
+++ insert_all.js
@@ -1,19 +1,23 @@
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('test2.db');
 
-let sql = [
+let sqls = [
   `insert into board ("name","maker_id") values ("FC", 1);`,
   `insert into board ("name","maker_id") values ("XC", 1);`,
   `insert into board ("name","maker_id") values ("ORCA", 1);`,
   `insert into board ("name","maker_id") values ("SPROUT", 1);`,
 ]
 
-db.serialize( () => {
-	db.run( sql, (error, row) => {
-		if(error) {
-			console.log('Error: ', error );
-			return;
-		}
-		console.log( "データを追加しました" );
-	});
-});
+for( let sql of sqls ){
+  db.serialize( ()=> {
+    db.run( sql, (error, row) => {
+		  if(error) {
+			  console.log('Error: ', error );
+			  return;
+		  }
+		  console.log( "データを追加しました" );
+	  });
+  });
+}
+
+  
