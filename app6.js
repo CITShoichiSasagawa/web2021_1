912d0fae0601e6c70850888d655d131d9cd8b434
Shoichi Sasagawa <115807586+CITShoichiSasagawa@users.noreply.github.com>
2022-11-04 08:21:36 +0000

    top作成　メーカーリンクからボード一覧に飛ぶように変更

--- app6.js
+++ app6.js
@@ -2,13 +2,13 @@ const express = require("express");
 const app = express();
 
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('test.db');
+const db = new sqlite3.Database('test2.db');
 
 app.set('view engine', 'ejs');
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  const message = "Hello world";
+  const message = "こんにちは";
   res.render('show', {mes:message});
 });
 
@@ -23,21 +23,41 @@ app.get("/db", (req, res) => {
     })
 })
 app.get("/top", (req, res) => {
-    //console.log(req.query.pop);    // ①
     let desc = "";
     if( req.query.desc ) desc = " desc";
     let sql = "select id, 都道府県, 人口 from example order by 人口" + desc + " limit " + req.query.pop + ";";
-    //console.log(sql);    // ②
     db.serialize( () => {
         db.all(sql, (error, data) => {
             if( error ) {
                 res.render('show', {mes:"エラーです"});
             }
-            //console.log(data);    // ③
             res.render('select', {data:data});
         })
     })
 })
+
+app.get("/maker", (req, res) => {
+    db.serialize( () => {
+        db.all("select id, name from maker;", (error, row) => {
+            if( error ) {
+                res.render('show', {mes:"エラーです"});
+            }
+            res.render('select2', {data:row});
+        })
+    })
+})
+
+app.get("/board", (req, res) => {
+    db.serialize( () => {
+        db.all("select id, name from board;", (error, row) => {
+            if( error ) {
+                res.render('show', {mes:"エラーです"});
+            }
+            res.render('select3', {data:row});
+        })
+    })
+})
+
 app.use(function(req, res, next) {
   res.status(404).send('ページが見つかりません');
 });
